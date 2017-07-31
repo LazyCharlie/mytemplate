46742c84fea9c5cd6a60a83838c1abe7c8787637
LazyCharlie <3376554599@qq.com>
2017-08-01 00:03:34 +0800

    加入了fft高精乘

--- BIG DIGIT/BigDigit.cpp
+++ BIG DIGIT/BigDigit.cpp
@@ -1,9 +1,76 @@
-#include<iostream>
-#include<algorithm>
-#include<cstdio>
+#include<bits/stdc++.h>
 using namespace std;
-#define SIZE 128
+#define SIZE 128      //注意：做大整数乘法运算时，开的数组大小不小于一个大于这两个数之和的最小二次幂
+//size开多大基本上对时间影响不大，除非_int数组开很大   预定义size主要是考虑空间复杂度优化
+const double PI = acos(-1.0);
+int ans[SIZE];//储存乘法的答案;
 bool isfail = false;
+//复数结构体
+struct cmplex
+{
+	double r, i;
+	cmplex(double _r = 0.0, double _i = 0.0)
+	{
+		r = _r; i = _i;
+	}
+	cmplex operator +(const cmplex &b)
+	{
+		return cmplex(r + b.r, i + b.i);
+	}
+	cmplex operator -(const cmplex &b)
+	{
+		return cmplex(r - b.r, i - b.i);
+	}
+	cmplex operator *(const cmplex &b)
+	{
+		return cmplex(r*b.r - i*b.i, r*b.i + i*b.r);
+	}
+}x1[SIZE], x2[SIZE];
+void change(cmplex y[], int len)
+{
+	int i, j, k;
+	for (i = 1, j = len / 2; i < len - 1; i++)
+	{
+		if (i < j)swap(y[i], y[j]);
+		//交换互为小标反转的元素，i<j保证交换一次
+		//i做正常的+1，j左反转类型的+1,始终保持i和j是反转的
+		k = len / 2;
+		while (j >= k)
+		{
+			j -= k;
+			k /= 2;
+		}
+		if (j < k) j += k;
+	}
+}
+/*
+* 做FFT 时,len必须为2^k形式，
+* on==1时是DFT，on==-1时是IDFT
+*/
+void fft(cmplex y[], int len, int on)
+{
+	change(y, len);
+	for (int h = 2; h <= len; h <<= 1)
+	{
+		cmplex wn(cos(-on * 2 * PI / h), sin(-on * 2 * PI / h));
+		for (int j = 0; j < len; j += h)
+		{
+			cmplex w(1, 0);
+			for (int k = j; k < j + h / 2; k++)
+			{
+				cmplex u = y[k];
+				cmplex t = w*y[k + h / 2];
+				y[k] = u + t;
+				y[k + h / 2] = u - t;
+				w = w*wn;
+			}
+		}
+	}
+	if (on == -1)
+		for (int i = 0; i < len; i++)
+			y[i].r /= len;
+}
+
 class _int {
 private:
 	char s[SIZE];
@@ -20,7 +87,12 @@ public:
 	template<class T>
 	T operator=(T _a);
 	
+	_int operator*(const _int &a);
+
 	operator bool();
+
+//	template<class T>
+//	_int operator/(T a);
 
 	bool cmp(const char *a, const char *b, int len);
 
@@ -30,11 +102,47 @@ public:
 
 	friend ostream& operator<<(ostream &os, const _int &a);
 
+
 	template<class T>
 	friend const _int operator+(T a, _int _b);
 
 	template<class T>
+	friend const _int operator*(T a, _int _b);
+
+	template<class T>
 	friend const _int operator-(T a, _int _b);
+
+	bool operator<=(const _int &a);
+
+	template<class T>
+	bool operator<=(T a);
+
+	template<class T>
+	friend bool operator<=(T a, const _int &b);
+
+	bool operator>=(const _int &a);
+
+	template<class T>
+	bool operator>=(T a);
+
+	template<class T>
+	friend bool operator>=(T a, const _int &b);
+
+	template<class T>
+	bool operator<(T a);
+
+	bool operator<(const _int &a);
+
+	template<class T>
+	friend bool operator<(T a, _int& b);
+
+	template<class T>
+	friend bool operator>(T a, _int &b);
+
+	template<class T>
+	bool operator>(T a);
+
+	bool operator>(const _int &a);
 
 	_int operator++(int);    //后置++
 
@@ -55,12 +163,17 @@ public:
 	const _int operator+(T a);
 
 	template<class T>
+	const _int operator*(T a);
+
+	template<class T>
 	void operator+=(T a);
 
 	template<class T>
 	void operator-=(T a);
 
 	void operator+=(_int &a);
+
+	void operator*=(const _int &a);
 
 	void operator-=(_int &a);
 
@@ -75,7 +188,7 @@ public:
 	bool operator==(_int &a);
 
 	~_int();
-};
+}tmp;//临时数据;
 
 _int::operator bool()
 {
@@ -93,8 +206,109 @@ bool _int::cmp(const char *a, const char *b, int len)
 }
 
 template<class T>
+const _int operator*(T a, _int _b)
+{
+	tmp = a;
+	return tmp*_b;
+}
+
+template<class T>
+bool _int::operator>(T a)
+{
+	tmp = a;
+	return *this > tmp;
+}
+
+bool _int::operator<=(const _int &a)
+{
+	return !(a < *this);
+}
+
+template<class T>
+bool operator>(T a, _int &b)
+{
+	tmp = a;
+	return tmp > b;
+}
+
+template<class T>
+bool operator<=(T a, const _int &b)
+{
+	tmp = a;
+	return tmp <= b;
+}
+
+template<class T>
+bool _int::operator>=(T a)
+{
+	tmp = a;
+	return *this >= a;
+}
+
+template<class T>
+bool _int::operator<=(T a)
+{
+	tmp = a;
+	return tmp <= *this;
+}
+
+bool _int::operator>(const _int &a)
+{
+	if (a.length > length)return false;
+	if (a.length < length)return true;
+	for (int i = length - 1; i >= 0; i--) {
+		if (a.s[i] > s[i])return false;
+		else if (a.s[i] < s[i])return true;
+	}
+	return false; //完全相等
+}
+
+bool _int::operator<(const _int &a)
+{
+	if (a.length > length)return true;
+	if (a.length < length)return false;
+	for (int i = length - 1; i >= 0; i--) {
+		if (a.s[i] > s[i])return true;
+		else if (a.s[i] < s[i])return false;
+	}
+	return false;
+}
+
+template<class T>
+bool operator<(T a,_int& b)
+{
+	tmp = a;
+	return tmp < b;
+}
+
+bool _int::operator>=(const _int &a)
+{
+	return !(a > *this);
+}
+
+template<class T>
+bool operator>=(T a, const _int &b)
+{
+	tmp = a;
+	return a >= b;
+}
+
+template<class T>
+bool _int::operator<(T a)
+{
+	tmp = a;
+	return *this < tmp;
+}
+
+template<class T>
 _int::_int(T _num) {
 	*this = _num;
+}
+
+template<class T>
+const _int _int::operator*(T a)
+{
+	return a*(*this);
 }
 
 void _int::operator-=(_int &a)
@@ -120,6 +334,37 @@ _int _int::operator--()
 	return *this;
 }
 
+void _int::operator*=(const _int &a)
+{
+	*this = *this*a;
+}
+
+_int _int::operator*(const _int &a)  //时间复杂度NlogN的大数乘法，假设相乘的两个数位数都是N;
+{
+	_int res;
+	int len = 1;
+	while (len < a.length + length + 1)len = len << 1;
+	for (int i = 0; i < a.length; i++)x1[i] = cmplex((double)(a.s[i] - '0'), 0);
+	for (int i = a.length; i < len; i++) x1[i] = cmplex(0, 0);
+	fft(x1, len, 1);
+	for (int i = 0; i < length; i++)x2[i] = cmplex((double)(s[i] - '0'), 0);
+	for (int i = length; i < len; i++) x2[i] = cmplex(0, 0);
+	fft(x2, len, 1);
+	for (int i = 0; i < len; i++) x1[i] = x1[i] * x2[i];
+	fft(x1, len, -1);
+	for (int i = 0; i < len; i++) ans[i] = (int)(x1[i].r + 0.5);
+	ans[len] = 0;
+	for (int i = 1; i < len; i++) {
+		ans[i] += ans[i - 1] / 10;
+		ans[i - 1] %= 10;
+	}
+	while (len>0 && !ans[len]) len--;
+	res.length = len + 1;
+	for (int i = len; i >= 0; i--)res.s[i] = ans[i] + '0';
+	if (a.sign && !sign || !a.sign&&sign)res.sign = true;
+	return res;
+}
+
 _int _int::operator++()
 {
 	*this += 1;
@@ -141,7 +386,7 @@ void _int::operator-=(T a)
 template<class T>
 const _int operator-(T a, _int _b)
 {
-	_int tmp = a;
+	tmp = a;
 	return tmp - _b;
 }
 
@@ -160,7 +405,7 @@ bool _int::operator==(_int &a)
 template<class T>
 void _int::operator+=(T a)
 {
-	_int tmp = a;
+	tmp = a;
 	*this = tmp + *this;
 }
 
@@ -185,7 +430,7 @@ const _int operator+(T a, _int _b)
 		_b.length = max(cnt, _b.length);
 	}
 	else {
-		_int tmp = k;     //将a升为_int 类型
+		tmp = k;     //将a升为_int 类型
 		_b = tmp + _b;
 	}
 	return _b;
@@ -282,6 +527,14 @@ bool operator!=(T a, const _int &b)
 	}
 	if (a)return true;
 	return false;
+}
+
+template<class T>
+_int _int::operator/(T a)
+{
+	tmp = a;
+	_int ans;
+
 }
 
 const _int _int::operator+(_int a)
@@ -392,9 +645,9 @@ _int a, b;
 //long long a,b;
 int main()
 {
-	a = 50;
-	while (--a) {
-		cout << a << endl;
+	while (cin>>a>>b) {
+		memset(ans, 0, sizeof(ans));
+		cout << a*b << endl;
 	}
 	//for (int i = 0; i < 10000; i++) {
 	//	int d = rand() % 18 + 1;
